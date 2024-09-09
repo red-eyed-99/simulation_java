357c7ce222fb4ce59a9528ffdd4c5cb06185a013
red-eyed-99 <mgnblack99@gmail.com>
2024-09-09 14:55:39 +0300

    feat(ui): add basic GUI elements with JavaFX

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,29 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        AreaGenerator generator = new AreaGenerator(new Area(10));
-        generator.generateArea();
+
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/ui/layouts/Main.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("Simulation");
+        stage.setWidth(1200);
+        stage.setHeight(800);
+
+        stage.show();
     }
 }
