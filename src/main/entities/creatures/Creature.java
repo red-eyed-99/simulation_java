96bf46240258408ee0ff6f0823b0b2aca3da4eb0
red-eyed-99 <mgnblack99@gmail.com>
2024-09-09 14:42:45 +0300

    feat: add creature view direction status

--- src/main/entities/creatures/Creature.java
+++ src/main/entities/creatures/Creature.java
@@ -1,5 +1,6 @@
 package main.entities.creatures;
 
+import main.CreatureViewDirection;
 import main.entities.Entity;
 
 import java.util.HashSet;
@@ -9,6 +10,8 @@ public abstract class Creature extends Entity {
     private int healthPoint;
     private int moveSpeed = 2;
     private int runSpeed;
+
+    public CreatureViewDirection viewDirection = CreatureViewDirection.RIGHT;
 
     private Set<Entity> pathToFood = new HashSet<>();
 
