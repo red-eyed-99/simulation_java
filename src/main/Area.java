9b09b226f0f5e2878d79ef79ad07ac265afcf198
red-eyed-99 <mgnblack99@gmail.com>
2024-09-10 11:49:47 +0300

    feat: add get entity logic

--- src/main/Area.java
+++ src/main/Area.java
@@ -7,11 +7,21 @@ import java.util.HashMap;
 
 public class Area {
     public final int size;
-    public final HashMap<Coordinates, Entity> entities;
+
+    public final HashMap<Coordinates, Entity> landscape;
     public final HashMap<Coordinates, Creature> creatures = new HashMap<>();
 
     public Area(int size) {
         this.size = size;
-        entities = new HashMap<>(size * 2);
+        landscape = new HashMap<>(size * 2);
+    }
+
+    public Entity getEntity(Coordinates coordinates) {
+        Creature creature = creatures.get(coordinates);
+        if (creature != null) {
+            return creature;
+        }
+
+        return landscape.get(coordinates);
     }
 }
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -12,7 +12,6 @@ import main.entities.landscape.surface.Ground;
 import main.ui.custom_controls.ZoomableScrollPane;
 
 import java.io.FileNotFoundException;
-import java.util.HashMap;
 
 public class MainController {
     private Simulation simulation;
@@ -42,7 +41,7 @@ public class MainController {
             for (int column = 0; column < area.size; column++) {
                 areaGrid.getColumnConstraints().add(new ColumnConstraints());
 
-                Entity entity = area.entities.get(new Coordinates(row, column));
+                Entity entity = area.getEntity(new Coordinates(row, column));
 
                 // ground rendering implements in style.css as background
                 if (entity instanceof Ground) {
