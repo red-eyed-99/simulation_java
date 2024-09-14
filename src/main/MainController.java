98e21687f77593f97490d94d4982e35eb96a630b
red-eyed-99 <mgnblack99@gmail.com>
2024-09-14 09:29:57 +0300

    feat(ui): add button for execution of the next simulation turn and deleting images of creatures by old coordinates

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,17 +1,20 @@
 package main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import main.entities.Entity;
+import main.entities.creatures.Creature;
 import main.entities.landscape.surface.Ground;
 import main.ui.custom_controls.ZoomableScrollPane;
-import java.util.Objects;
+
+import java.util.*;
 
 public class MainController {
     private Simulation simulation;
@@ -21,6 +24,9 @@ public class MainController {
 
     @FXML
     private ZoomableScrollPane zoomableScrollPane;
+
+    @FXML
+    private Button nextTurnButton;
 
     @FXML
     private void initialize() {
@@ -60,12 +66,48 @@ public class MainController {
         return new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream(pathToImage))));
     }
 
-    public Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
-        for (Node node : gridPane.getChildren()) {
-            if (node instanceof ImageView && GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
-                return node;
+    public ImageView getAreaGridCellCreatureImage(int col, int row) {
+        List<ImageView> images = new ArrayList<>();
+
+        for (Node node : areaGrid.getChildren()) {
+            if (node instanceof ImageView imageView
+                    && GridPane.getColumnIndex(node) == col
+                    && GridPane.getRowIndex(node) == row){
+                images.add(imageView);
             }
         }
-        return null;
+
+        if (!images.isEmpty()) {
+            return images.getLast();
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    @FXML
+    private void nextTurnButtonClick(ActionEvent event) {
+        Set<Coordinates> currentCreaturesCoordinates = new HashSet<>(simulation.getArea().getCreatures().keySet());
+
+        Set<Coordinates> oldCreaturesCoordinates = new HashSet<>(currentCreaturesCoordinates.size());
+
+        for (Coordinates coordinates : currentCreaturesCoordinates) {
+            oldCreaturesCoordinates.add(new Coordinates(coordinates.x, coordinates.y));
+        }
+
+        simulation.nextTurn();
+
+        updateAreaGridCreaturesPositions(oldCreaturesCoordinates);
+    }
+
+    private void updateAreaGridCreaturesPositions(Set<Coordinates> oldCreaturesCoordinates) {
+        for (Coordinates coordinates : oldCreaturesCoordinates) {
+            areaGrid.getChildren().remove(getAreaGridCellCreatureImage(coordinates.x, coordinates.y));
+        }
+
+        Map<Coordinates, Creature> newCreaturesLocation = new HashMap<>(simulation.getArea().getCreatures());
+
+        for (Map.Entry<Coordinates, Creature> entry : newCreaturesLocation.entrySet()) {
+            areaGrid.add(getEntityImage(entry.getValue()), entry.getKey().x, entry.getKey().y);
+        }
     }
 }
