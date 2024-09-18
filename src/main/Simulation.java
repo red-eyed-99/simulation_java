b8d3cca2b728c2ae117d8d5dfefa0f0a1075bf58
red-eyed-99 <mgnblack99@gmail.com>
2024-09-18 10:02:53 +0300

    feat: add the ability to control the simulation process

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -17,39 +17,32 @@ public class Simulation {
 
     private List<Action> turnActions = new ArrayList<>();
 
-    private SimulationObserver observer;
-
     private Map<Coordinates, LandscapeEntity> landscapeBeforeRedraw;
     private Map<Coordinates, Creature> creaturesBeforeRedraw;
 
-    SimulationThread simulationThread;
+    private SimulationObserver observer;
+
+    private SimulationThread simulationThread;
 
     public Simulation(Area area, AreaGenerator areaGenerator) {
         this.area = area;
         this.areaGenerator = areaGenerator;
 
         setActions();
+
         updateEntitiesBeforeRedraw();
+
         simulationThread = new SimulationThread();
         simulationThread.setDaemon(true);
-
-    }
-
-    public void addObserver(SimulationObserver observer) {
-        this.observer = observer;
-    }
-
-    private void notifyLandscapeUpdated(Map<Coordinates, LandscapeEntity> oldLandscape) {
-        observer.onAreaLandscapeUpdated(oldLandscape);
-    }
-
-    private void notifyCreaturesUpdated(Map<Coordinates, Creature> oldCreatures, Map<Coordinates, LandscapeEntity> oldLandscape) {
-        observer.onAreaCreaturesUpdated(oldCreatures, oldLandscape);
     }
 
     private void setActions() {
         turnActions.add(new CreaturesAction(area));
         turnActions.add(new AddEntityAction(area, areaGenerator));
+    }
+
+    public void addObserver(SimulationObserver observer) {
+        this.observer = observer;
     }
 
     public Area getArea() {
@@ -64,9 +57,16 @@ public class Simulation {
         turnActions.get(0).execute();
         notifyObserverForRedraw();
         updateEntitiesBeforeRedraw();
+
+        movesCount++;
     }
 
     public void startSimulation() {
+        if (simulationThread.paused) {
+            simulationThread.resumeSimulation();
+            return;
+        }
+
         simulationThread.start();
     }
 
@@ -74,56 +74,68 @@ public class Simulation {
         simulationThread.pause();
     }
 
-    private void updateEntitiesBeforeRedraw() {
-        landscapeBeforeRedraw = Map.copyOf(area.getLandscapeEntities());
-        creaturesBeforeRedraw = Map.copyOf(area.getCreatures());
-    }
-
     private void notifyObserverForRedraw() {
         notifyLandscapeUpdated(landscapeBeforeRedraw);
         notifyCreaturesUpdated(creaturesBeforeRedraw, landscapeBeforeRedraw);
     }
 
-    class SimulationThread extends Thread {
+    private void notifyLandscapeUpdated(Map<Coordinates, LandscapeEntity> oldLandscape) {
+        observer.onAreaLandscapeUpdated(oldLandscape);
+    }
+
+    private void notifyCreaturesUpdated(Map<Coordinates, Creature> oldCreatures, Map<Coordinates, LandscapeEntity> oldLandscape) {
+        observer.onAreaCreaturesUpdated(oldCreatures, oldLandscape);
+    }
+
+    private void updateEntitiesBeforeRedraw() {
+        landscapeBeforeRedraw = Map.copyOf(area.getLandscapeEntities());
+        creaturesBeforeRedraw = Map.copyOf(area.getCreatures());
+    }
+
+    private class SimulationThread extends Thread {
+        volatile boolean paused = false;
+
+        Task<Void> simulationTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                while (true) {
+
+                    while (paused) {
+                        Thread.sleep(1000);
+                    }
+
+                    turnActions.get(1).execute();
+
+                    Platform.runLater(Simulation.this::notifyObserverForRedraw);
+
+                    Thread.sleep(50);
+
+                    updateEntitiesBeforeRedraw();
+
+                    turnActions.get(0).execute();
+
+                    Platform.runLater(Simulation.this::notifyObserverForRedraw);
+
+                    Thread.sleep(500);
+
+                    updateEntitiesBeforeRedraw();
+
+                    movesCount++;
+                }
+            }
+        };
+
         @Override
         public void run() {
             simulationTask.run();
         }
 
-        public void pause() {
-            simulationTask.cancel();
-//            try {
-//                sleep(900000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
+        void pause() {
+            paused = true;
+        }
+
+        void resumeSimulation() {
+            paused = false;
         }
     }
-
-    Task<Void> simulationTask = new Task<>() {
-        @Override
-        protected Void call() throws Exception {
-            while (true) {
-                turnActions.get(1).execute();
-
-                Platform.runLater(() -> {
-                    notifyObserverForRedraw();
-                });
-
-                Thread.sleep(100);
-
-                updateEntitiesBeforeRedraw();
-
-                turnActions.get(0).execute();
-
-                Platform.runLater(() -> {
-                    notifyObserverForRedraw();
-                });
-
-                Thread.sleep(500);
-
-                updateEntitiesBeforeRedraw();
-            }
-        }
-    };
 }
